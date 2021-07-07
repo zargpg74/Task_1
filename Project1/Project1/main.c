bbbfe4ae41a693989203b1c061373ab2d688f22e
Zakhar Gogolev <zargog74@gmail.com>
2021-07-07 21:33:32 +0500

    работает

--- Project1/Project1/main.c
+++ Project1/Project1/main.c
@@ -31,29 +31,25 @@ void main()
 	system("cls");
 	setlocale(0, "ru");
 	
-	while (1)
+	printf("Выберите вариант перевода:\n1) Из строки в число\n2) Из числа в строку\n3) Выход\nВаш вариант: ");
+	int choice;		
+	scanf_s("%d", &choice);
+	switch (choice)
 	{
-		printf("Выберите вариант перевода:\n1) Из строки в число\n2) Из числа в строку\n3) Выход\nВаш вариант: ");
-		int choice;
+	case 1:
+		str_number();
+		break;
+	case 2:
+		number_str();
+		break;
+	case 3:
+		exit(0);
+	default:
+		printf("Такого варианта не существует, попробуйте еще раз: ");
 		scanf_s("%d", &choice);
-		switch (choice)
-		{
-		case 1:
-			str_number();
-			break;
-		case 2:
-			number_str();
-			break;
-		case 3:
-			exit(0);
-		default:
-			printf("Такого варианта не существует, попробуйте еще раз: ");
-			scanf_s("%d", &choice);
-			Choice(choice);
-		}
-
-		printf("\n---------------------------------\n");
+		Choice(choice);
 	}
 
+	printf("\n");
 	system("pause");
 }
