2fc68bd5a499808684db1edce1bfe0b171580269
Zakhar Gogolev <zargog74@gmail.com>
2021-07-07 15:38:52 +0500

    Все работает

--- Project1/Project1/str.c
+++ Project1/Project1/str.c
@@ -15,6 +15,35 @@ int Error_input_str(int syst)//проверка на корректность введенных данных
 	}
 }
 
+char* trans_str(int* mass_number, int size)
+{
+	//массивы соотвествий для больших систем счисления
+	char* low = "abcdefghijklmnopqrstuvwxyz";
+	char* big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int* number_low[26] = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35 };
+	int* number_big[26] = { 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61 };
+
+	char* str = (char*)malloc(size * sizeof(char));//строка 
+
+	for (int i = 0; i < 10; i++)
+	{
+		if ((mass_number[i] >= 0) && (mass_number[i] <= 9))
+			str[i] = mass_number[i] + 48;
+		else
+		{
+			for (int j = 0; j < 26; j++)
+			{
+				if (mass_number[i] == number_low[j])
+					str[i] = low[j];
+				if (mass_number[i] == number_big[j])
+					str[i] = big[j];
+			}
+		}
+	}
+
+	return(str);
+}
+
 
 int number_str()
 {
@@ -28,5 +57,23 @@ int number_str()
 
 	Error_input_str(syst);
 
+	//перевод в указанную систему счисления
+	int* mass_number = NULL;//промежуточный массив
+	int size = 0;
+	while (number % syst < number)
+	{
+		mass_number = (int*)realloc(mass_number, (size + 1) * sizeof(int));
+		mass_number[size] = number % syst;
+		number /= syst;
+		size++;
+	}
+	mass_number[size] = number;
+	size++;
 
+	char* str = (char*)malloc(size * sizeof(char));
+	str = trans_str(mass_number, size);
+
+	printf("Строка: ");
+	for (int i = size - 1; i >= 0; i--)
+		printf("%c", str[i]);
 }
